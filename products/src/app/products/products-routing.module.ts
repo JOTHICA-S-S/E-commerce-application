fcae6f7a251d5dbaf56bc3f9288ff9477789059c
Jothica <jothicass@gmail.com>
2024-08-09 18:11:42 +0530

    Exposed remote module

--- products/src/app/products/products-routing.module.ts
+++ products/src/app/products/products-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsModule } from './products.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"", component:ProductsModule
+}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

f1107fc8cae2473d0adf4e7cc8725e00689b1939
Jothica <jothicass@gmail.com>
2024-08-26 14:42:10 +0530

    Have added start and home componene tfor better handling

--- home/src/app/app-routing.module.ts
+++ home/src/app/app-routing.module.ts
@@ -12,31 +12,39 @@ import { CollectionsComponent } from './collections/collections.component';
 import { deactiveAuthGuard } from './deactive-auth.guard';
 import { childAuthGuard } from './child-auth.guard';
 import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
+import { StartComponent } from './start/start.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:"Ecommerce", pathMatch:"full"},
-  {path:"Ecommerce",component:EcommComponent},
+  {path:'',redirectTo:"start", pathMatch:"full"},
+  {path:"start",component:StartComponent},
   {path:"SignIn",component:SignInComponent,canDeactivate:[deactiveAuthGuard]},
-  {path:"SignUp",component:SignUpComponent}, 
-  {path:"cart",component:CartComponent}, 
-  {path:"Products",component:ProductsListComponent,canActivate:[authGuard]},
-  // {
-  //   path:"products-mfe",
-  //     loadChildren:()=>
-  //       loadRemoteModule({
-  //         remoteEntry:"http://localhost:5200/remoteEntry.js",
-  //         exposedModule:"./ProductsModule",
-  //         type:"module"
-  //       }).then(m=>m.ProductsModule),
-  //       canActivate:[authGuard]
-      
-  // }
-  {path:"Trending",component:TrendingComponent, canActivateChild:[childAuthGuard],
+  {path:"SignUp",component:SignUpComponent},
+  {path:"home",component:HomeComponent,
     children:[
-      {path:"shops", component:ShopsComponent},//      http://localhost:5100/Trending/shops
-      {path:"collections", component:CollectionsComponent}  //  http://localhost:5100/Trending/collections
+      {path:"Ecommerce",component:EcommComponent},
+      {path:"cart",component:CartComponent}, 
+      {path:"Products",component:ProductsListComponent,canActivate:[authGuard]},
+      // {
+      //   path:"products-mfe",
+      //     loadChildren:()=>
+      //       loadRemoteModule({
+      //         remoteEntry:"http://localhost:5200/remoteEntry.js",
+      //         exposedModule:"./ProductsModule",
+      //         type:"module"
+      //       }).then(m=>m.ProductsModule),
+      //       canActivate:[authGuard]
+          
+      // }
+      {path:"Trending",component:TrendingComponent, canActivateChild:[childAuthGuard],
+        children:[
+          {path:"shops", component:ShopsComponent},//      http://localhost:5100/Trending/shops
+          {path:"collections", component:CollectionsComponent}  //  http://localhost:5100/Trending/collections
+        ]
+      }
     ]
-  },
+  }
+
   
 ];
 
